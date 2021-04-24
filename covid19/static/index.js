53844dff495819b549ffd55f86151109f9bf6d83
unknown <ynarula678@gmail.com>
2021-04-24 22:29:52 +0530

    JS updates will be done tomm

--- covid19/static/index.js
+++ covid19/static/index.js
@@ -1,24 +1,7 @@
-var loader;
-function loadNow(opacity) {
-    if(opacity <=0){
-        displayContent();
-    }
-    else{
-        loader.style.opacity = opacity;
-        window.setTimeout(function(){
-            loadNow(opacity - 0.05)
-        },100);
-    }
-}
-function displayContent(){
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-}
-document.addEventListener('DOMContentLoaded', function(){
-    loader=document.getElementById('loader');
-    loadNow(1);
-});
-
+$(window).on('load', function(){
+  $('.preloadeer').addClass('complete')
+})
+//I will do the work tomm asap
 !(function($) {
     "use strict";
   
